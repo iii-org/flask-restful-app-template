5aed14d70beda32325938adc1fe4b6425da7beb8
Romulus Urakagi Tsai <urakagi@gmail.com>
2021-01-26 10:00:02 +0800

    71 env

--- app/main.py
+++ app/main.py
@@ -10,17 +10,25 @@ TODOS = {
     '3': {'task': 'profit!'},
 }
 
+
 def abort_if_todo_doesnt_exist(todo_id):
     if todo_id not in TODOS:
         abort(404, message="Todo {} doesn't exist".format(todo_id))
 
 
 def dump(obj):
-  for attr in dir(obj):
-    print("obj.%s = %r" % (attr, getattr(obj, attr)))
+    for attr in dir(obj):
+        print("obj.%s = %r" % (attr, getattr(obj, attr)))
+
 
 parser = reqparse.RequestParser()
 parser.add_argument('task')
+
+
+class Root(Resource):
+    def get(self):
+        return "Hello World!"
+
 
 # Todo
 #   show a single todo item and lets you delete them
@@ -41,38 +49,41 @@ class Todo(Resource):
         return {"id": todo_id, "task": task}, 201
 
 
-class  GetTodo(Resource):
+class GetTodo(Resource):
     def post(self):
         print(reqparse.request.values.to_dict())
         parser.add_argument('id')
-        args= parser.parse_args()
+        args = parser.parse_args()
         abort_if_todo_doesnt_exist(args['id'])
         return TODOS[args['id']]
+
 
 # TodoList
 #   shows a list of all todos, and lets you POST to add new tasks
 class TodoList(Resource):
     def get(self):
         return TODOS
+
     def post(self):
         args = parser.parse_args()
         todo_id = '%d' % (len(TODOS) + 1)
         TODOS[todo_id] = {'task': args['task']}
-        return {"id": todo_id, "data": args['task'] }, 201
+        return {"id": todo_id, "data": args['task']}, 201
+
 
 class ServerInfo(Resource):
     def get(self):
         return {"serverInfo": "flask api v2 is working"}
 
+
 ##
 ## Actually setup the Api resource routing here
 ##
-api.add_resource(ServerInfo,'/serverinfo')
+api.add_resource(Root, '/')
+api.add_resource(ServerInfo, '/serverinfo')
 api.add_resource(TodoList, '/todos')
-api.add_resource(GetTodo,'/gettodo')
+api.add_resource(GetTodo, '/gettodo')
 api.add_resource(Todo, '/todos/<string:todo_id>')
-
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
-
